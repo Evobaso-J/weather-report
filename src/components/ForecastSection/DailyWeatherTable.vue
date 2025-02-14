f2ae5dc9b4aa787b46f7e11fbe47cb60c1739f9b
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2025-02-14 11:54:52 +0100

    feat: use daily weather table

--- src/components/ForecastSection/DailyWeatherTable.vue
+++ src/components/ForecastSection/DailyWeatherTable.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    {{ dailyWeather }}
-  </div>
+  <UTable
+    :columns
+    :rows
+  />
 </template>
 
 <script setup lang='ts'>
-import type { HourlyWeather } from '~/entities/weather/types'
+import type { HourlyWeather, WeatherVariable } from '~/entities/weather/types'
 
 defineComponent({ name: 'DailyWeatherTable' })
 type DailyWeatherProps = {
   dailyWeather: HourlyWeather[]
 }
-defineProps<DailyWeatherProps>()
+const props = defineProps<DailyWeatherProps>()
+
+type WeatherColumns = {
+  key: keyof HourlyWeather
+  label?: string
+}
+
+const columns: WeatherColumns[] = [
+  {
+    key: 'time',
+    // label: 'Hour',
+  },
+  {
+    key: 'rain',
+    label: 'Rain',
+  },
+  {
+    key: 'temperature2m',
+    label: 'Temperature',
+  },
+  {
+    key: 'precipitationProbability',
+    label: 'Precipitation Probability',
+  },
+]
+
+const parseVariableToStringWithUnit = (variable: WeatherVariable<unknown>) => {
+  return `${variable.value}${variable.unit}`
+}
+
+const filterPastHours = (weather: HourlyWeather) => {
+  return weather.time >= new Date()
+}
+
+const rows = computed(() => props.dailyWeather
+  .filter(filterPastHours)
+  .map(weather => ({
+    time: new Date(weather.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+    rain: parseVariableToStringWithUnit(weather.rain),
+    temperature2m: parseVariableToStringWithUnit(weather.temperature2m),
+    precipitationProbability: parseVariableToStringWithUnit(weather.precipitationProbability),
+  })))
 </script>
