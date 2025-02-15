64d0974e397e0c51ee918f80d6e8d1f770f5c4ee
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2025-02-15 12:35:23 +0100

    feat: add tabbed view to home

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,40 @@
 <template>
-  <UCard class="w-full">
-    <ForecastSection
-      v-if="dataStatus === 'success'"
-      :weather-forecast="weatherForecast ?? []"
-    />
-    <DataAlternateStatus
-      v-else
-      :status="dataStatus"
-    />
-  </UCard>
+  <section class="flex flex-col items-start">
+    <UTabs
+      :items="tabs"
+      @change="changeTab"
+    >
+      <template #icon="{ item, selected }">
+        <UIcon
+          :name="item.icon"
+          class="w-4 h-4 flex-shrink-0 me-2"
+          :class="[selected && 'text-primary-500']"
+        />
+      </template>
+      <template #default="{ item, selected }">
+        <span
+          class="truncate"
+          :class="[selected && 'text-primary-500']"
+        >{{ item.label }}</span>
+      </template>
+    </UTabs>
+    <UCard class="w-full">
+      <DataAlternateStatus
+        v-if="dataStatus !== 'success'"
+        :status="dataStatus"
+      />
+      <ForecastSection
+        v-else-if="activeTab.key === 'forecast'"
+        :weather-forecast="weatherForecast ?? []"
+      />
+    </UCard>
+  </section>
 </template>
 
 <script setup lang="ts">
 import { FORECAST_DAYS } from '~/entities/weather/constants'
 import { weatherRepository } from '~/entities/weather/repository'
+import type { TabItem } from '#ui/types'
 
 const { t } = useI18n()
 
@@ -24,6 +45,25 @@ useHead({
 const { currentCity } = useCityStore()
 
 const { unwrapResult } = useUnwrapResult()
+
+const tabs = [{
+  label: t('weatherForecast'),
+  icon: 'i-mdi-weather-partly-rainy',
+  key: 'forecast',
+},
+{
+  label: t('weatherHistory'),
+  icon: 'i-mdi-weather-cloudy-clock',
+  key: 'history',
+}] as const satisfies (TabItem & { key: string })[]
+
+const activeTab = ref<(typeof tabs)[number]>(tabs[0])
+
+const changeTab = (index: number) => {
+  const newTab = tabs[index]
+  if (!newTab) return
+  activeTab.value = newTab
+}
 
 const weatherRepo = weatherRepository()
 const { data: weatherForecast, status } = useAsyncData(() => weatherRepo.query({
