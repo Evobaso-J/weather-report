170acbb52a450194a77c7fe974bbf6381608507e
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2025-02-15 17:15:59 +0100

    fix: use only timespans util

--- src/entities/weatherHistory/utils.test.ts
+++ src/entities/weatherHistory/utils.test.ts
@@ -1,6 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import { formatWeatherHistoryQueryParams, type StartEndDateQueryParams, type TimeSpan } from './utils'
-import { WEATHER_HISTORY_QUERY_PARAMS } from './constants'
+import { getTimeSpans } from './utils'
 
 const fakeSystemTime = new Date('2021-09-09T00:00:00Z')
 
@@ -13,41 +12,26 @@ describe('weatherHistory utils', () => {
     vi.useRealTimers()
   })
 
-  describe('formatWeatherHistoryQueryParams', () => {
-    const timeSpans: {
-      span: TimeSpan
-      expected: StartEndDateQueryParams & {
-        hourly?: typeof WEATHER_HISTORY_QUERY_PARAMS
-        daily?: typeof WEATHER_HISTORY_QUERY_PARAMS
-      } }[] = [
-      {
-        span: 'weekly',
-        expected: {
+  describe('getTimeSpans', () => {
+    it('should return the correct query when the timespan is $span', () => {
+      const timeSpans = getTimeSpans()
+
+      const expectedTimespans = {
+        weekly: {
           start_date: '2021-09-02',
           end_date: '2021-09-09',
-          hourly: WEATHER_HISTORY_QUERY_PARAMS,
         },
-      },
-      { span: 'monthly',
-        expected: {
+        monthly: {
           start_date: '2021-08-09',
           end_date: '2021-09-09',
-          daily: WEATHER_HISTORY_QUERY_PARAMS,
         },
-      },
-      {
-        span: 'yearly',
-        expected: {
+        yearly: {
           start_date: '2020-09-09',
           end_date: '2021-09-09',
-          daily: WEATHER_HISTORY_QUERY_PARAMS,
         },
-      },
-    ]
+      }
 
-    it.each(timeSpans)('should return the correct query when the timespan is $span', ({ span, expected }) => {
-      const query = { latitude: 0, longitude: 0, timeSpan: span }
-      expect(formatWeatherHistoryQueryParams(query)).toMatchObject(expected)
+      expect(timeSpans).toMatchObject(expectedTimespans)
     })
   })
 })
--- src/entities/weatherHistory/utils.ts
+++ src/entities/weatherHistory/utils.ts
@@ -1,5 +1,3 @@
-import { WEATHER_HISTORY_QUERY_PARAMS } from './constants'
-
 const formatDate = (date: Date): string => {
   const year = date.getFullYear()
   const month = String(date.getMonth() + 1).padStart(2, '0')
@@ -16,29 +14,9 @@ const formatPastTimeSpan = (dateNumber: number): StartEndDateQueryParams => {
   }
 }
 
-const getTimeSpans = () => ({
+export const getTimeSpans = () => ({
   weekly: formatPastTimeSpan(new Date().setDate(new Date().getDate() - 7)),
   monthly: formatPastTimeSpan(new Date().setMonth(new Date().getMonth() - 1)),
   yearly: formatPastTimeSpan(new Date().setFullYear(new Date().getFullYear() - 1)),
 })
 export type TimeSpan = keyof ReturnType<typeof getTimeSpans>
-
-const getPerTimeParams = (time: TimeSpan) => {
-  if (time === 'weekly') {
-    return { hourly: WEATHER_HISTORY_QUERY_PARAMS }
-  }
-  return { daily: WEATHER_HISTORY_QUERY_PARAMS }
-}
-
-export type WeatherHistoryRepoQueryParams = { latitude: number, longitude: number, timeSpan: TimeSpan }
-export const formatWeatherHistoryQueryParams = (query: WeatherHistoryRepoQueryParams) => {
-  const { timeSpan, ...queryRest } = query
-  const dates = getTimeSpans()[timeSpan]
-  const perTimeParams = getPerTimeParams(timeSpan)
-
-  return {
-    ...queryRest,
-    ...perTimeParams,
-    ...dates,
-  }
-}
