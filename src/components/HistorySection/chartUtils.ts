b93fbfb1a02676f7b6c75e0c393a851577cd07af
Evobaso-J <57828270+Evobaso-J@users.noreply.github.com>
2025-02-16 23:56:40 +0100

    feat: abstract chart utils

---
+++ src/components/HistorySection/chartUtils.ts
@@ -0,0 +1,40 @@
+import { format } from 'date-fns/format'
+import type { DailyWeather } from '~/entities/weatherHistory/types'
+
+const getUTCMonth = (date: Date): Date => {
+  return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1))
+}
+
+type AggregatorFunction = (val1: number, val2: number) => number
+export const aggregateDailyWeatherByMonth = (data: DailyWeather[], aggregator: AggregatorFunction): DailyWeather[] => {
+  const months = new Map<string, DailyWeather>()
+
+  data.forEach((dailyWeather) => {
+    const { time, ...nonTimeParams } = dailyWeather
+    const monthAndYear = format(new Date(time), 'MMM yyyy')
+    const dataPerMonth = months.get(monthAndYear)
+
+    if (!dataPerMonth) {
+      months.set(monthAndYear, {
+        time: getUTCMonth(time),
+        ...nonTimeParams,
+      })
+    }
+    else {
+      const nonTimeParamsKeys = Object.keys(nonTimeParams) as (keyof typeof nonTimeParams)[]
+      const aggregateData = nonTimeParamsKeys.reduce((acc, key) => {
+        acc[key] = {
+          value: aggregator(dataPerMonth[key].value, dailyWeather[key].value),
+          unit: dailyWeather[key].unit,
+        }
+        return acc
+      }, {} as Omit<DailyWeather, 'time'>)
+
+      months.set(monthAndYear, {
+        time: getUTCMonth(time),
+        ...aggregateData,
+      })
+    }
+  })
+  return Array.from(months.values())
+}
